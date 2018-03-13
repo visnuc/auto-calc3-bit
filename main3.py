8f414b6c1e6d7c562e2750b9fd82731dae9a65a3
Visnu Pritom Chowdhury <36967726+pr330m@users.noreply.github.com>
2018-03-13 19:50:44 +0600

    Update main3.py

--- main3.py
+++ main3.py
@@ -5,8 +5,9 @@ choice = str(input("Choose from the list :\
 \n [2] Severe Sepsis\
 \n [3] Severe Dehydration\
 \n [4] Some Dehydration\
-\n [5] MAP calculation\
-\n[1/2/3/4/5]: "))
+\n [5] `X'% dehydration\
+\n [6] MAP calculation\
+\n[1/2/3/4/5/6]: "))
 if choice == "1":
     from hyperna3 import hyperna_msg3
     from hyperna3 import hyperna_calc3
@@ -17,6 +18,8 @@ elif choice == "3":
 elif choice == "4":
     from somedh3 import somedh3
 elif choice == "5":
+    from xdh3 import xdh3
+elif choice == "6":
     from map_bp3 import map_bp3
 else:
     print("Please try again.")
