72715888fdae662929635aae75262453886fd602
Visnu Pritom Chowdhury <36967726+pr3370m@users.noreply.github.com>
2018-06-29 20:35:54 +0600

    Update diet3.py

--- diet3.py
+++ diet3.py
@@ -1,11 +1,18 @@
 def diet3():
-    wt = float(input("Weight [kg]: "))
-    each_feed = float(input("Each feed [ml]: "))
-    interval = float(input("Interval [hr]: "))
+    wt = float(input("  Weight (kg): "))
+    each_feed = float(input("Feed vol (ml): "))
+    interval = float(input("Interval (hr): "))
     times = 24/interval
     t_vol = (each_feed*times)/wt
-    kind = str(input("MIF/MS/MS100/FSRS [1|2|3|4]: "))
-    if kind == "1":
+    kind = str(input(" (1) MIF\
+\n (2) MS\
+\n (3) MS100\
+\n (4) FSRS\
+\n (5) 3/4 FSRS\
+\n (6) Soyabased formula\
+\n (7) 3/4 Soyabased formula\
+\nEnter code: "))
+    if kind == "1" or kind == "6":
         t_cal = t_vol*0.68
     elif kind == "2":
         t_cal = t_vol*0.67
@@ -13,7 +20,13 @@ def diet3():
         t_cal = t_vol*1.0
     elif kind == "4":
         t_cal = t_vol*0.70
-    print("\nDaily dietary volume:", t_vol, "ml/kg/day")
-    print("Daily calorie intake:", t_cal, "kcal/kg/day\n")
+    elif kind == "5":
+        t_cal = t_vol*0.566
+    elif kind == "7":
+        t_cal = t_vol*0.51
+    else:
+        print("Please try again.")
+    print("\nVolume :", round(t_vol, 2), "ml/kg/day")
+    print("Calorie:", round(t_cal, 2), "kcal/kg/day \n")
 
 diet3()
