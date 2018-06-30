c8448e6338d6ee076f4570bc611f6362a4d57d2e
Visnu Pritom Chowdhury <36967726+pr3370m@users.noreply.github.com>
2018-06-30 21:40:00 +0600

    Update somedh3.py

--- somedh3.py
+++ somedh3.py
@@ -2,12 +2,12 @@ from message3 import message3
 
 def somedh3():
   wt = float(input("Weight (kg): "))
-  sam = str(input("SAM-SUW / Non-SAM [1/2]: "))
+  sam = str(input("SAM-SUW(1)/ Non-SAM(2) ?\nEnter code: "))
   if sam == "1":
-    print("\n", wt*10, "ml/hr for 2 hrs, \
-then\n", wt*5, "ml/hr for 8-10 hrs (GORS)\n")
-  elif sam == "2": 
-    print("\n", wt*75, "ml over 4-6 hrs (GORS)\n")
+    print("\n", round(wt*10, 2), "ml/hr for 2 hrs, \
+then\n", round(wt*5, 2), "ml/hr for 8-10 hrs (GORS)\n")
+  elif sam == "2":
+    print("\n", round(wt*75, 2), "ml over 4-6 hrs (GORS)\n")
   else:
     print(" \nPlease try again.")
 
