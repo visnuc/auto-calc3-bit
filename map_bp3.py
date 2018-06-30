c5190d9d69f451e90361e553d0ed681d1d83ab02
Visnu Pritom Chowdhury <36967726+pr3370m@users.noreply.github.com>
2018-06-30 21:37:09 +0600

    Update map_bp3.py

--- map_bp3.py
+++ map_bp3.py
@@ -3,10 +3,11 @@ from message3 import message3
 def map_bp3():
     sbp = float(input(" Systolic BP (mmHg): "))
     dbp = float(input("Diastolic BP (mmHg): "))
-    map_bp3 = (dbp*2 + sbp)/3
+    map_bp = (dbp*2 + sbp)/3
     # MAP formula 2:
-    # map_bp3 = ((sbp-dbp)/3)+dbp
-    print("\n MAP is", map_bp3, "mmHg\n")
+    # map_bp = ((sbp-dbp)/3)+dbp
+    print("\nMAP is", round(map_bp, 2), "mmHg\
+\n[Reference (mmHg): Child >50; Adult >65] \n")
 
 map_bp3()
 #input("Please press `Enter' to exit")
