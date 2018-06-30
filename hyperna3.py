277af269077a1ad0b837086cd0fb6a11ae95e191
Visnu Pritom Chowdhury <36967726+pr3370m@users.noreply.github.com>
2018-06-30 21:33:43 +0600

    Update hyperna3.py

--- hyperna3.py
+++ hyperna3.py
@@ -1,26 +1,28 @@
 from message3 import message3
 
 def hyperna_msg3():
-    print("REMEMBER:")
     print("Sodium contents: ")
-    print(" GORS: 75 mmol/l ")
+    print(" G-ORS : 75 mmol/l ")
     print(" 1/2 NS: 77 mmol/l")
-    print(" 1/2 Acetate: 66 mmol/l \n")
+    print(" 1/2 AC: 66 mmol/l \n")
 
 def hyperna_calc3():
     print("FLUID VOLUME CALCULATION")
-    wt = input(" Weight (kg): ")
+    wt = float(input(" Weight (kg): "))
     serum = input(" Serum Sodium (mmol/l): ")
-    fluid = input(" Sodium in correction fluid (mmol/l): ")
+    fluid = input(" Fluid Sodium (mmol/l): ")
 
     corr = ((10/((float(serum)-float(fluid))/((float(wt)*0.6)+1)))*1000)/24
-    print("\nCorrection volume:", corr, "ml/hr\n")
+    print("\nCorrection volume:", round(corr, 2), "ml/hr\n")
 
     print("25% DIET CURTAIL")
-    diet = input(" Enter full diet vol (ml/2h):  ")
-    curt = float(diet)*0.75 
-    print(" \n25% curtailed volume", curt, "ml/2h + Salt added.")
+    kwash = str(input(" Marasmus(1) | Kwash(2): "))
+    if kwash == "1":
+        curt = (wt*10)*0.75
+    elif kwash == "2":
+        curt = (wt*9)*0.75
+    print("\nCurtailed diet:", round(curt, 2), "ml/2h + Salt added.")
 
 hyperna_msg3()
 hyperna_calc3()
-#raw_input("Press `Enter' to exit.")
+#input("Press `Enter' to exit.")
