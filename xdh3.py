4788abe43442f80657375b0bfaa0f8636c63ecc0
Visnu Pritom Chowdhury <36967726+pr3370m@users.noreply.github.com>
2018-07-05 00:25:09 +0600

    Update xdh3.py

--- xdh3.py
+++ xdh3.py
@@ -2,7 +2,14 @@ def xdh3():
     x = float(input("\nPercentage(%) of dehydration (number only): "))
     wt = float(input("Weight (kg): "))
     xdh_corr = wt*(x*10)
-    print(round(xdh_corr, 2), "ml in 4-6 hrs")
+    print(round(xdh_corr, 2), "ml in 4-6 hrs\n")
+    ques = str(input("Want to do anything else? (y/n): "))
+    if ques == "y" or ques == "Y":
+        from main3 import main3
+    else:
+        print("Thanks for using auto-calc!")
+        exit()
+        
 
 xdh3()
 
